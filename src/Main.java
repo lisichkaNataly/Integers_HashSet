6c54398b8ec2939e7b0b3bf296e7457a0d9f904b
Izmaylova <Natalyatf@yandex.ru>
2022-11-19 22:13:01 +0500

    Домашнее задание 2 Множество целых чисел Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        numbersSet();
+    }
+
+    private static void numbersSet() {
+        Set<Integer> numbers = new HashSet<>(20);
+        Random random = new Random();
+        while (numbers.size() < 20) {
+            numbers.add(random.nextInt(1000));
+        }
+        System.out.println(numbers);
+        Iterator<Integer> numberIterator = numbers.iterator();
+        while (numberIterator.hasNext()) {
+            int number = numberIterator.next();
+            if (number % 2 != 0) {
+                numberIterator.remove();
+            }
+        }
+        System.out.println(numbers);
     }
 }
